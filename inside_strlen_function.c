41e1aa7635076f80507630aad443ff5a396f7b64
Nevzat Seferoğlu <43863606+nevzatseferoglu@users.noreply.github.com>
2019-01-28 02:46:16 +0300

    Add files via upload

---
+++ inside_strlen_function.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int fonk(char *s);   //output=17 
+int main(void){
+
+	int i;
+	char arr[]="Final Examination"; //example array which can be everything that you want ;
+	printf("%d\n",fonk(arr)); 
+	return 0;
+}
+int fonk(char *s){   				//This fucntion returns the size of sitring array type ;
+	char *p; 
+	p=s;
+	while((*p)!='\0')
+	{
+		++p;
+	}
+	return p-s;
+}
