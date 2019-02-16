71e91599bac9128b1b056ea9affae5183203aa57
Nevzat Seferoğlu <43863606+nevzatseferoglu@users.noreply.github.com>
2019-02-16 15:32:38 +0300

    Add files via upload
    Differences between char Array and char Pointer type ;

---
+++ specialExample.c
@@ -0,0 +1,17 @@
+char special(May Anything) //We can discuss whether it is true or not.
+{
+	{
+	char arr[10]="zybran"
+	above the code is equal to below one;
+	}
+
+
+	{
+	char *ptr;
+	ptr=(char*)malloc(10*sizeof(char));
+	free(ptr);
+	}
+
+	When We create an "char array" in some sort of function ;
+	It will occuring a process that is overhead.
+}
